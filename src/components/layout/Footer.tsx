adbb0491494b00222de5c6f8c98434de112f0b28
Andres <advallejouc@cesde.net>
2025-09-30 06:34:02 -0500

    Comienzo footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,10 @@
-export default function Footer() {
-return (
-<footer className="bg-blue-950 text-white mt-8">
-<section id="mapa" className="content mapa scroll-mt-24">
-<iframe
-title="Sedes Kingdom Barber"
-src="https://www.google.com/maps/d/embed?mid=1fE4auR4b1fRD0ut4VGMrG8i1-C4DpJ8&ehbc=2E312F"
-width="100%"
-height="250"
-loading="lazy"
-className="w-full border-0"
-/>
-</section>
-  <div className="text-center py-4">Kingdom Barber ®2024</div>
-</footer>
-);
+import React from 'react';
+
+// Interface para las sedes
+interface Sede {
+  nombre: string;
+  direccion: string;
+  telefono: string;
+  horario: string;
 }
+
