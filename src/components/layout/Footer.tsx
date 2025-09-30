4c95aa65edccb2f1b1f0fd909fccdd851f62c2c5
Andres <advallejouc@cesde.net>
2025-09-30 06:45:05 -0500

    Ajuste tamaño casillas sedes

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -68,7 +68,7 @@ return (
           <h3 className="text-2xl font-bold text-center mb-8 text-yellow-400">
             Nuestras Sedes
           </h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-5">
             {sedes.map((sede, index) => (
               <div 
                 key={index} 
